3fb638c915d539ec8e0275b07bc9d9ed9774eb15
krsachin9696 <krsachin9696@gmail.com>
2024-12-21 14:21:55 +0530

    basic stack navigation setup

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/Home';
+import ChatScreen from './screens/ChatScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={Home}/>
+        <Stack.Screen name='ChatScreen' component={ChatScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
